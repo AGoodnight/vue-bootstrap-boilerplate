c1eefd808146ed028e9b39a50fbfb5c115720c83
AGoodnight <chaofoam@gmail.com>
2019-04-26 10:59:24 -0400

    Added Webpack

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './assets/sass/main.scss';
+import Vue from "vue";
+const app = new Vue({
+  el: "#app",
+  data: {
+    message: "Hello Vue!"
+  }
+});
+
+console.log("hello")
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry:'./src/index.js',
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+            process.env.NODE_ENV !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
+            "css-loader", // translates CSS into CommonJS
+            "sass-loader" // compiles Sass to CSS, using Node Sass by default
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    }),
+    new MiniCssExtractPlugin({
+        // Options similar to the same options in webpackOptions.output
+        // both options are optional
+        filename: "[name].css",
+        chunkFilename: "[id].css"
+    })
+  ],
+  resolve: {
+    alias: {
+      vue$:"vue/dist/vue.esm.js"
+    }
+  }
+};
