8f9d3deae07513f4771645d3efc46ac26c4e7601
AGoodnight <chaofoam@gmail.com>
2019-04-26 15:55:54 -0400

    Started making some mock layout

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <div class="navbar navbar-expand-lg fixed-top navbar-dark bg-primary">
+    <div class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
         <router-link to="/" class="navbar-brand">Home</router-link>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
@@ -10,14 +10,9 @@
           <ul class="navbar-nav">
 
             <li class="nav-item">
-              <router-link class="nav-link" to="/blog">Blog</router-link>
+              <router-link class="nav-link" to="/about">About Us</router-link>
             </li>
-            <li class="nav-item">
-            <router-link class="nav-link" to="/services">Services</router-link>
-            </li>
-            <li class="nav-item">
-             <router-link class="nav-link" to="/contact">contact</router-link>
-            </li>
+
           </ul>
         </div>
         </div>
@@ -26,7 +21,6 @@
        <transition name="moveInUp">
          <router-view/>
        </transition>
-
       </div>
   </div>
 </template>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,43 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <h1 class='mt-4 display-4'>Welcome to Vue!</h1>
+    <p class="lead mt-3 mb-4">
+      Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus.
+    </p>
+    <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <p>
+        <dl class="row">
+          <dt class="col-sm-3">Description lists</dt>
+          <dd class="col-sm-9">A description list is perfect for defining terms.</dd>
+
+          <dt class="col-sm-3">Euismod</dt>
+          <dd class="col-sm-9">
+            <p>Vestibulum id ligula porta felis euismod semper eget lacinia odio sem nec elit.</p>
+            <p>Donec id elit non mi porta gravida at eget metus.</p>
+          </dd>
+
+          <dt class="col-sm-3">Malesuada porta</dt>
+          <dd class="col-sm-9">Etiam porta sem malesuada magna mollis euismod.</dd>
+
+          <dt class="col-sm-3 text-truncate">Truncated term is truncated</dt>
+          <dd class="col-sm-9">Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</dd>
+
+          <dt class="col-sm-3">Nesting</dt>
+          <dd class="col-sm-9">
+            <dl class="row">
+              <dt class="col-sm-4">Nested definition list</dt>
+              <dd class="col-sm-8">Aenean posuere, tortor sed cursus feugiat, nunc augue blandit nunc.</dd>
+            </dl>
+          </dd>
+        </dl>
+      </p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+
+    </div>
+  </div>
   </div>
 </template>
 
