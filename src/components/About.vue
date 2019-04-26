26ef6eb31cd38d77fc7337acebbd1ce71340f82b
AGoodnight <chaofoam@gmail.com>
2019-04-26 16:09:30 -0400

    Started using Events and Conditional Rendering on About.vue

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,14 +1,39 @@
 <template>
   <div class="about">
-    <h1>About</h1>
+    <h1 v-bind:id="headerID">{{msg}}</h1>
+    <h2>The current time is {{now}}</h2>
+    <ol>
+      <li v-for="item in items">
+        {{item}}
+      </li>
+    </ol>
+    <button v-on:click="addName">Add a Name</button>
   </div>
 </template>
 
 <script>
   export default {
     name: 'about',
+    computed: {
+    now: function () {
+        return Date.now()
+      }
+    },
+    methods:{
+      addName(){
+        this.items.push("New Name")
+      }
+    },
     data () {
       return {
+        items:[
+          "Bilbo",
+          "Gandalf",
+          "Greyworm",
+          "Danny",
+          "Samwise"
+        ],
+        headerID: "about-us-header",
         msg: 'This is an About Page'
       }
     }
